36f5d11e3e9dd7ec3e2e4394629196837bcd3cbf
Sachin Kumara Liyanage <sachinkumaraliyanage@gmail.com>
2020-04-23 12:22:27 +0530

    Add comment to locater file

--- lib/Utill/locator.dart
+++ lib/Utill/locator.dart
@@ -5,12 +5,11 @@ import 'CRUDModel.dart';
 
 //sachin kumara Liyanage
 //IT17152938
-//test comment
 GetIt locator = GetIt.instance;
 
 final String firebaseImageUrl = 'gs://mediscope-cfcfc.appspot.com';
 final String firebaseImageMainFolder = 'images/';
-
+//locate Firebase connection
 void setupLocator() {
   locator.registerLazySingleton(() => FirebaseDataApi('Medicine'));
   locator.registerLazySingleton(() => CRUDModel());
