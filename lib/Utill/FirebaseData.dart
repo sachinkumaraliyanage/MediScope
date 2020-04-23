9abc8745ec27483323dc775a3ec2e7091abc7452
Sachin Kumara Liyanage <sachinkumaraliyanage@gmail.com>
2020-04-23 12:20:50 +0530

    Add comment to FirebaseDataApi class

--- lib/Utill/FirebaseData.dart
+++ lib/Utill/FirebaseData.dart
@@ -8,6 +8,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 //sachin kumara Liyanage
 //IT17152938
 
+//middleware for CRUDModel and Medicine modal
 class FirebaseDataApi{
 
   static String useremail="";
@@ -23,27 +24,32 @@ class FirebaseDataApi{
     ref = _db.collection(path);
   }
 
+  //fetch all Medicines from Firebase
   Future<QuerySnapshot> getDataCollection() {
     print(FirebaseDataApi.useremail);
     return ref.orderBy("date",descending: true).where("email",isEqualTo: FirebaseDataApi.useremail).getDocuments() ;
   }
 
+  //fetch all Medicines from Firebase as Stream
   Stream<QuerySnapshot> streamDataCollection() {
     return ref.snapshots() ;
   }
 
+  //get Medicines by ID
   Future<DocumentSnapshot> getDocumentById(String id) {
     return ref.document(id).get();
   }
 
+  //remove Medicines
   Future<void> removeDocument(String id){
     return ref.document(id).delete();
   }
 
+  //edit Medicines
   Future<DocumentReference> addDocument(Map data) {
     return ref.add(data);
   }
-
+  //add Medicines
   Future<void> updateDocument(Map data , String id) {
     return ref.document(id).updateData(data) ;
   }
